91eb71c2f58a42c772bcd56516b8a986b97ef7b3
NikitaFadin <110337575+NikitaFadin@users.noreply.github.com>
2022-08-07 16:31:59 +0300

    как то так

--- Module3/home_work/string/[hw]03_task_string.py
+++ Module3/home_work/string/[hw]03_task_string.py
@@ -1,3 +1,15 @@
 # Посчитайте количество согласных букв в данной строке.
 
 text = ...
+word = "Imprastion"
+vowels = 0
+consonants = 0
+for i in word:
+    letter = i.lower()
+    if letter == "a" or letter == "e" or\
+       letter == "i" or letter == "o" or\
+       letter == "u" or letter == "y":
+        vowels += 1
+    else:
+        consonants += 1
+print("Vowels %i\nConsonants %i" % (vowels, consonants))
