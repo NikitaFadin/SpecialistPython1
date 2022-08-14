61ee5cb11ae29f92e3e24986e528c50fe8dff6e5
NikitaFadin <110337575+NikitaFadin@users.noreply.github.com>
2022-08-14 12:47:56 +0300

    Update and rename Module5/practice/repeat/02_task_repeat.py to Модуль5/практика/повторить/02_task_repeat.py

--- Модуль5/практика/повторить/02_task_repeat.py
+++ Модуль5/практика/повторить/02_task_repeat.py
@@ -5,3 +5,19 @@
 
 def palindrome(number):
     pass
+
+number = str(input())
+x = len(number)
+i = 0
+x = x - 1
+k = 0
+while x - i >= i:
+    if number[x - i] == number[i]:
+        i += 1
+    else:
+        k = 1
+        break
+if k == 1:
+  print("no")
+else:
+  print("yes")
